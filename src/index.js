4c959c317e90ed3bc3799e0cf0bd7bda59667294
JoelFlachsbart <joel.flachsbart@gmail.com>
2018-12-25 15:52:40 -0800

    added world scene

--- src/index.js
+++ src/index.js
@@ -1,34 +1,164 @@
 import 'phaser';
 
+var BootScene = new Phaser.Class({
+ 
+    Extends: Phaser.Scene,
+ 
+    initialize:
+ 
+    function BootScene ()
+    {
+        Phaser.Scene.call(this, { key: 'BootScene' });
+    },
+ 
+    preload: function ()
+    {
+        // load the resources here
+    },
+ 
+    create: function ()
+    {
+        this.scene.start('WorldScene');
+    }
+});
+ 
+var WorldScene = new Phaser.Class({
+ 
+    Extends: Phaser.Scene,
+ 
+    initialize:
+ 
+    function WorldScene ()
+    {
+        Phaser.Scene.call(this, { key: 'WorldScene' });
+    },
+    preload: function ()
+    {
+        this.load.image('tiles', 'assets/map/main_sheet.png');
+
+        this.load.tilemapTiledJSON('map', 'assets/map/ybsim_map.json');
+
+        this.load.spritesheet(
+            'player',
+            'assets/characters.png',
+            {frameWidth: 16, frameHeight: 16}
+        )
+        
+    },
+    create: function ()
+    {
+        let map = this.make.tilemap({ key: 'map'});
+        let tiles = map.addTilesetImage('ybsim_tiles', 'tiles');
+        let grass = map.createStaticLayer(0, tiles, 0, 0);
+        let obstacles = map.createStaticLayer(1, tiles, 0, 0);
+        obstacles.setCollisionByExclusion([-1]);
+
+        this.player = this.physics.add.sprite(50, 100, 'player', 10);
+        this.physics.world.bounds.width = map.widthInPixels;
+        this.physics.world.bounds.height = map.heightInPixels;
+        this.player.setCollideWorldBounds(true);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.roundPixels = true;
+
+        //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('player', { frames: [10, 18, 10, 26]}),
+            frameRate: 10,
+            repeat: -1
+        });
+        
+        // animation with key 'right'
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('player', { frames: [10, 18, 10, 26]}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'up',
+            frames: this.anims.generateFrameNumbers('player', { frames: [11, 19, 11, 27]}),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'down',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 9, 17, 9, 25 ] }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.physics.add.collider(this.player, obstacles);
+    },
+    update: function (time, delta)
+{
+	this.player.body.setVelocity(0);
+ 
+        // Horizontal movement
+        if (this.cursors.left.isDown)
+        {
+            this.player.body.setVelocityX(-80);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.body.setVelocityX(80);
+        }
+ 
+        // Vertical movement
+        if (this.cursors.up.isDown)
+        {
+            this.player.body.setVelocityY(-80);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.body.setVelocityY(80);
+        }    
+
+        if (this.cursors.left.isDown)
+        {
+            this.player.anims.play('left', true);
+            this.player.flipX = true;
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.anims.play('right', true);
+            this.player.flipX = false;
+        }
+        else if (this.cursors.up.isDown)
+        {
+            this.player.anims.play('up', true);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.anims.play('down', true);
+        }
+        else
+        {
+            this.player.anims.stop();
+        }
+}
+});
+ 
 var config = {
     type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: {
-        preload: preload,
-        create: create
-    }
+    parent: 'content',
+    width: 320,
+    height: 240,
+    zoom: 2,
+    pixelArt: true,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+    },
+    scene: [
+        BootScene,
+        WorldScene
+    ]
 };
-
 var game = new Phaser.Game(config);
-
-function preload ()
-{
-    this.load.image('logo', 'assets/logo.png');
-}
-
-function create ()
-{
-    var logo = this.add.image(400, 150, 'logo');
-
-    this.tweens.add({
-        targets: logo,
-        y: 450,
-        duration: 2000,
-        ease: 'Power2',
-        yoyo: true,
-        loop: -1
-    });
-
-}
