20b74a122f807da2c5c488da3ac2a0e960943b5a
JoelFlachsbart <joel.flachsbart@gmail.com>
2018-12-27 20:54:34 -0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,34 @@
 import 'phaser';
 
 var BootScene = new Phaser.Class({
- 
+
     Extends: Phaser.Scene,
- 
+
     initialize:
- 
-    function BootScene ()
-    {
-        Phaser.Scene.call(this, { key: 'BootScene' });
-    },
- 
-    preload: function ()
-    {
+
+        function BootScene() {
+            Phaser.Scene.call(this, { key: 'BootScene' });
+        },
+
+    preload: function () {
         // load the resources here
     },
- 
-    create: function ()
-    {
+
+    create: function () {
         this.scene.start('WorldScene');
     }
 });
- 
+
 var WorldScene = new Phaser.Class({
- 
+
     Extends: Phaser.Scene,
- 
+
     initialize:
- 
-    function WorldScene ()
-    {
-        Phaser.Scene.call(this, { key: 'WorldScene' });
-    },
-    preload: function ()
-    {
+
+        function WorldScene() {
+            Phaser.Scene.call(this, { key: 'WorldScene' });
+        },
+    preload: function () {
         this.load.image('tiles', 'assets/map/main_sheet.png');
 
         this.load.tilemapTiledJSON('map', 'assets/map/ybsim_map.json');
@@ -41,13 +36,12 @@ var WorldScene = new Phaser.Class({
         this.load.spritesheet(
             'player',
             'assets/characters.png',
-            {frameWidth: 16, frameHeight: 16}
+            { frameWidth: 16, frameHeight: 16 }
         )
-        
+
     },
-    create: function ()
-    {
-        let map = this.make.tilemap({ key: 'map'});
+    create: function () {
+        let map = this.make.tilemap({ key: 'map' });
         let tiles = map.addTilesetImage('ybsim_tiles', 'tiles');
         let grass = map.createStaticLayer(0, tiles, 0, 0);
         let obstacles = map.createStaticLayer(1, tiles, 0, 0);
@@ -67,82 +61,90 @@ var WorldScene = new Phaser.Class({
         //  animation with key 'left', we don't need left and right as we will use one and flip the sprite
         this.anims.create({
             key: 'left',
-            frames: this.anims.generateFrameNumbers('player', { frames: [10, 18, 10, 26]}),
+            frames: this.anims.generateFrameNumbers('player', { frames: [10, 18, 10, 26] }),
             frameRate: 10,
             repeat: -1
         });
-        
+
         // animation with key 'right'
         this.anims.create({
             key: 'right',
-            frames: this.anims.generateFrameNumbers('player', { frames: [10, 18, 10, 26]}),
+            frames: this.anims.generateFrameNumbers('player', { frames: [10, 18, 10, 26] }),
             frameRate: 10,
             repeat: -1
         });
         this.anims.create({
             key: 'up',
-            frames: this.anims.generateFrameNumbers('player', { frames: [11, 19, 11, 27]}),
+            frames: this.anims.generateFrameNumbers('player', { frames: [11, 19, 11, 27] }),
             frameRate: 10,
             repeat: -1
         });
         this.anims.create({
             key: 'down',
-            frames: this.anims.generateFrameNumbers('player', { frames: [ 9, 17, 9, 25 ] }),
+            frames: this.anims.generateFrameNumbers('player', { frames: [9, 17, 9, 25] }),
             frameRate: 10,
             repeat: -1
         });
 
         this.physics.add.collider(this.player, obstacles);
+
+        this.spawns = this.physics.add.group({ classType: Phaser.GameObjects.Zone });
+        for (var i = 0; i < 30; i++) {
+            var x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+            var y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+            // parameters are x, y, width, height
+            this.spawns.create(x, y, 20, 20);
+        }
+        this.physics.add.overlap(this.player, this.spawns, this.onMeetEnemy, false, this);
     },
-    update: function (time, delta)
-{
-	this.player.body.setVelocity(0);
- 
+    update: function (time, delta) {
+        this.player.body.setVelocity(0);
+
         // Horizontal movement
-        if (this.cursors.left.isDown)
-        {
+        if (this.cursors.left.isDown) {
             this.player.body.setVelocityX(-80);
         }
-        else if (this.cursors.right.isDown)
-        {
+        else if (this.cursors.right.isDown) {
             this.player.body.setVelocityX(80);
         }
- 
+
         // Vertical movement
-        if (this.cursors.up.isDown)
-        {
+        if (this.cursors.up.isDown) {
             this.player.body.setVelocityY(-80);
         }
-        else if (this.cursors.down.isDown)
-        {
+        else if (this.cursors.down.isDown) {
             this.player.body.setVelocityY(80);
-        }    
+        }
 
-        if (this.cursors.left.isDown)
-        {
+        if (this.cursors.left.isDown) {
             this.player.anims.play('left', true);
             this.player.flipX = true;
         }
-        else if (this.cursors.right.isDown)
-        {
+        else if (this.cursors.right.isDown) {
             this.player.anims.play('right', true);
             this.player.flipX = false;
         }
-        else if (this.cursors.up.isDown)
-        {
+        else if (this.cursors.up.isDown) {
             this.player.anims.play('up', true);
         }
-        else if (this.cursors.down.isDown)
-        {
+        else if (this.cursors.down.isDown) {
             this.player.anims.play('down', true);
         }
-        else
-        {
+        else {
             this.player.anims.stop();
         }
-}
-});
+    },
+    onMeetEnemy: function(player, zone) {        
+        // we move the zone to some other location
+        zone.x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+        zone.y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);       
  
+        this.cameras.main.shake(300);
+
+        // start battle 
+    }
+});
+
 var config = {
     type: Phaser.AUTO,
     parent: 'content',
@@ -153,7 +155,8 @@ var config = {
     physics: {
         default: 'arcade',
         arcade: {
-            gravity: { y: 0 }
+            gravity: { y: 0 },
+            debug: true
         }
     },
     scene: [
